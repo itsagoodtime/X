6d05f8ed5530e2897fdc3beaebbec0d32478f492
itsagoodtime <yougotwhat@yeah.net>
2025-03-09 03:19:52 +0800

    Update Test.lua

--- Games/Test.lua
+++ Games/Test.lua
@@ -1,3 +1,4 @@
 local s = xRequire('Services.lua','Models')
+local s2 = xRequire('Signal.lua','Models')
 local p = s:Get('Players')
-print('just a test',p.LocalPlayer)
+print('just a test',p.LocalPlayer,s2)
